7a5b36507e20713f362ff1dc2c549d22dde0a2da
ashvets <Sheila01>
2014-10-24 11:15:54 -0400

    Bug fixes

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -9,21 +9,33 @@ require 'acceptance_test'
 require 'turnip/capybara'
 require 'gnawrnip'
 
-config_name = File.expand_path("spec/acceptance_config.yml")
-config = config_name ? HashWithIndifferentAccess.new(YAML.load_file(config_name)) : {}
-
-AcceptanceTest.instance.configure(config)
-
-# AcceptanceTest.instance.configure(webapp_url: 'http://www.wikipedia.org')
-# AcceptanceTest.instance.register_driver(:webkit)
-# AcceptanceTest.instance.register_driver(:poltergeist)
-
-AcceptanceTest.instance.configure_turnip 'tmp/report.html'
-
 require 'steps/search_with_drivers_steps'
 require 'steps/search_with_pages_steps'
 require 'steps/search_with_scenario_outline_steps'
 require 'steps/search_with_table_steps'
+
+acceptance_test = AcceptanceTest.instance
+
+RSpec.configure do |conf|
+  conf.before(:type => :feature) do
+    config_name = File.expand_path("spec/acceptance_config.yml")
+    config = config_name ? HashWithIndifferentAccess.new(YAML.load_file(config_name)) : {}
+
+    acceptance_test.configure(config)
+
+    # acceptance_test.configure(webapp_url: 'http://www.wikipedia.org')
+    # acceptance_test.register_driver(:webkit)
+    # acceptance_test.register_driver(:poltergeist)
+
+    acceptance_test.configure_turnip 'tmp/report.html'
+
+    acceptance_test.setup
+  end
+
+  conf.after(:type => :feature) do
+    acceptance_test.teardown
+  end
+end
 
 
 
