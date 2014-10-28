074772b9d71594f43b6d992ac9e15dcb6bc1b71a
ashvets <Sheila01>
2014-10-28 10:50:12 -0400

    Bug fix

--- lib/acceptance_test/screenshot_maker.rb
+++ lib/acceptance_test/screenshot_maker.rb
@@ -6,25 +6,30 @@ class ScreenshotMaker
   end
 
   def make page, options
-    page.save_screenshot(screenshot_path(options))
+    name = screenshot_name(File.basename(options[:file_path]), options[:line_number])
+    path = File.expand_path("#{basedir}/#{name}")
+
+    page.save_screenshot(path)
   end
 
-  def screenshot_path options
-    File.expand_path("#{basedir}/#{screenshot_name(options)}")
-  end
-
-  def screenshot_name options
-    filename = File.basename(options[:file_path])
-    line_number = options[:line_number]
-
-    name = filename
-    name += "-#{line_number}" if line_number
-
-    "#{name}.png"
+  def screenshot_name filename, line_number=nil
+    "#{filename}#{line_number ? '-'+line_number : ''}.png"
   end
 
   def screenshot_url options
-    "file:///#{screenshot_path(options)}"
+    file_path = options[:file_path]
+
+    if file_path =~ /http:/ or file_path =~ /https:/ or file_path =~ /ftp:/
+      name = screenshot_name(File.basename(options[:file_path]), options[:line_number])
+
+      "#{file_path}/#{name}"
+    else
+      name = screenshot_name(File.basename(options[:file_path]), options[:line_number])
+
+      path = File.expand_path("#{basedir}/#{name}")
+
+      "file:///#{path}"
+    end
   end
 
 end
